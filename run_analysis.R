f74af28ccfde38c03cadeeab03265d7c23c2751b
Esa Jaakola <epen.tunnus@gmail.com>
2015-08-23 09:57:21 +0300

    Readable column names added to dataset

--- run_analysis.R
+++ run_analysis.R
@@ -63,10 +63,34 @@ analyse <- function() {
         if(x == 5) filtered[i,1] <- "STANDING"
         if(x == 6) filtered[i,1] <- "LAYING"
     }
-    filtered
-##    4. Appropriately labels the data set with descriptive variable names.
-    ## Name columns
 
+##    4. Appropriately labels the data set with descriptive variable names.
+    ## Add readable column names into data frame using data from features.txt file
+    colnames(filtered) <- c("Activity", ## 1
+                      "Subject", ## 2
+                    "tBodyAcc-mean()-X", "tBodyAcc-mean()-Y", "tBodyAcc-mean()-Z", ## 3-5
+                    "tBodyAcc-std()-X", "tBodyAcc-std()-Y", "tBodyAcc-std()-Z", ## 6-8
+                    "tGravityAcc-mean()-X", "tGravityAcc-mean()-Y", "tGravityAcc-mean()-Z", ## 9-11
+                    "tGravityAcc-std()-X", "tGravityAcc-std()-Y", "tGravityAcc-std()-Z", ## 12-14                      
+                    "tBodyAccJerk-mean()-X", "tBodyAccJerk-mean()-Y", "tBodyAccJerk-mean()-Z", ## 15-17
+                    "tBodyAccJerk-std()-X", "tBodyAccJerk-std()-Y", "tBodyAccJerk-std()-Z", ## 18-20
+                    "tBodyGyro-mean()-X", "tBodyGyro-mean()-Y", "tBodyGyro-mean()-Z", ## 21-23
+                    "tBodyGyro-std()-X", "tBodyGyro-std()-Y", "tBodyGyro-std()-Z", ## 24-26
+                    "tBodyGyroJerk-mean()-X", "tBodyGyroJerk-mean()-Y", "tBodyGyroJerk-mean()-Z", ## 27-29
+                    "tBodyGyroJerk-std()-X", "tBodyGyroJerk-std()-Y", "tBodyGyroJerk-std()-Z", ## 30-32
+                    "tBodyAccMag-mean()", "tBodyAccMag-std()", "tGravityAccMag-mean()", "tGravityAccMag-std()", ## 33-36
+                    "tBodyAccJerkMag-mean()", "tBodyAccJerkMag-std()", "tBodyGyroMag-mean()", "tBodyGyroMag-std()", ## 37-40
+                    "tBodyGyroJerkMag-mean()", "tBodyGyroJerkMag-std()", ## 41-42
+                    "fBodyAcc-mean()-X", "fBodyAcc-mean()-Y", "fBodyAcc-mean()-Z", ## 43-45
+                    "fBodyAcc-std()-X", "fBodyAcc-std()-Y", "fBodyAcc-std()-Z", ## 46-48
+                    "fBodyAccJerk-mean()-X", "fBodyAccJerk-mean()-Y", "fBodyAccJerk-mean()-Z", ## 49-51
+                    "fBodyAccJerk-std()-X", "fBodyAccJerk-std()-Y", "fBodyAccJerk-std()-Z", ## 52-54
+                    "fBodyGyro-mean()-X", "fBodyGyro-mean()-Y", "fBodyGyro-mean()-Z", ## 55-57
+                    "fBodyGyro-std()-X", "fBodyGyro-std()-Y", "fBodyGyro-std()-Z", ## 58-60
+                    "fBodyAccMag-mean()", "fBodyAccMag-std()", "fBodyBodyAccJerkMag-mean()", "fBodyBodyAccJerkMag-std()", ## 61-64
+                    "fBodyBodyGyroMag-mean()", "fBodyBodyGyroMag-std()", "fBodyBodyGyroJerkMag-mean()", "fBodyBodyGyroJerkMag-std()") ## 65-68
+
+    filtered
 ##    5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and $
     ## Create new data structure
 
